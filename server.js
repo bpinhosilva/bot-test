6f16c0fd668bd112af5aebe5630985d9d3035fe1
Bruno Pinho <bpinhosilva@gmail.com>
2017-04-19 02:04:57 -0300

    Added package.json and sever.js files.

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.get("/test", function (req, res) {
+    res.send("Ok");
+});
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
